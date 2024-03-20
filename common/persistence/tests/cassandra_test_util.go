64f3a84c0e1e0a128f20d4be961c4e1830c1b4f8
Christian Mauduit (DataDog) <christian.mauduit@datadoghq.com>
2024-03-20 16:13:10 +0100

    [cassandra] add a way to customize how cassandra sessions are created
    (cherry picked from commit 947fcf56dc2b330fada4fe5e0de698d8ac4fc125)

--- common/persistence/tests/cassandra_test_util.go
+++ common/persistence/tests/cassandra_test_util.go
@@ -52,7 +52,7 @@ import (
 const (
 	// TODO hard code this dir for now
 	//  need to merge persistence test config / initialization in one place
-	testCassandraExecutionSchema  = "../../../schema/cassandra/temporal/schema.cql"
+	testCassandraExecutionSchema = "../../../schema/cassandra/temporal/schema.cql"
 )
 
 // TODO merge the initialization with existing persistence setup
@@ -105,6 +105,7 @@ func SetUpCassandraDatabase(cfg *config.Cassandra, logger log.Logger) {
 		func() (*gocql.ClusterConfig, error) {
 			return commongocql.NewCassandraCluster(adminCfg, resolver.NewNoopResolver())
 		},
+		nil,
 		logger,
 		metrics.NoopMetricsHandler,
 	)
@@ -133,6 +134,7 @@ func ApplySchemaUpdate(cfg *config.Cassandra, schemaFile string, logger log.Logg
 		func() (*gocql.ClusterConfig, error) {
 			return commongocql.NewCassandraCluster(*cfg, resolver.NewNoopResolver())
 		},
+		nil,
 		logger,
 		metrics.NoopMetricsHandler,
 	)
@@ -168,6 +170,7 @@ func TearDownCassandraKeyspace(cfg *config.Cassandra) {
 		func() (*gocql.ClusterConfig, error) {
 			return commongocql.NewCassandraCluster(adminCfg, resolver.NewNoopResolver())
 		},
+		nil,
 		log.NewNoopLogger(),
 		metrics.NoopMetricsHandler,
 	)
