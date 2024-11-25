a9448808138954c974afb6707c2beee116cd1ba6
Hardy Ferentschik <hardy@ferentschik.de>
2024-11-25 09:10:31 +0100

    [WFENG-2874] Ensures CheckCompatibleVersion uses DD custom gocql driver

--- common/persistence/cassandra/version_checker.go
+++ common/persistence/cassandra/version_checker.go
@@ -25,7 +25,10 @@
 package cassandra
 
 import (
+	"fmt"
+
 	"github.com/gocql/gocql"
+
 	"go.temporal.io/server/common/config"
 	"go.temporal.io/server/common/log"
 	"go.temporal.io/server/common/metrics"
@@ -83,11 +86,16 @@ func CheckCompatibleVersion(
 	expectedVersion string,
 ) error {
 
+	createSession, err := getCreateSessionFunc(cfg.CreateSessionFunc)
+	if err != nil {
+		return fmt.Errorf("unable to get create session func: %v", err)
+	}
+
 	session, err := commongocql.NewSession(
 		func() (*gocql.ClusterConfig, error) {
 			return commongocql.NewCassandraCluster(cfg, r)
 		},
-		commongocql.CreateSession,
+		createSession,
 		log.NewNoopLogger(),
 		metrics.NoopMetricsHandler,
 	)
