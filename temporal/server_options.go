5bff15a0d7b4d61176af3c33545c1a6a7bac1e1d
modernplumbing <celia.yuen@datadoghq.com>
2025-02-28 08:30:22 -0500

    [WFENG-3357] Pass in custom internal frontend claimmapper

--- temporal/server_options.go
+++ temporal/server_options.go
@@ -45,6 +45,7 @@ type (
 		authorizer                   authorization.Authorizer
 		tlsConfigProvider            encryption.TLSConfigProvider
 		claimMapper                  authorization.ClaimMapper
+		internalFrontendClaimMapper  InternalFrontendClaimMapper
 		audienceGetter               authorization.JWTAudienceMapper
 		persistenceServiceResolver   resolver.ServiceResolver
 		elasticsearchHttpClient      *http.Client
