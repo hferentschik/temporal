8a058d3dee0098b0343263670f7a2d5ac29d5758
Hardy Ferentschik <hardy@ferentschik.de>
2025-07-25 18:30:06 +0200

    [WFENG-3519] Migrates Temporal to AWS SDK v2
    mod tidy
    
    Signed-off-by: Hardy Ferentschik <hardy@ferentschik.de>

--- temporal/fx.go
+++ temporal/fx.go
@@ -124,6 +124,7 @@ type (
 		ClientFactoryProvider client.FactoryProvider
 		DynamicConfigClient   dynamicconfig.Client
 		TLSConfigProvider     encryption.TLSConfigProvider
+		EsConfig              *esclient.Config
 		EsClient              esclient.Client
 		MetricsHandler        metrics.Handler
 	}
@@ -311,6 +312,7 @@ func ServerOptionsProvider(opts []ServerOption) (serverOptionsProvider, error) {
 		ClientFactoryProvider: clientFactoryProvider,
 		DynamicConfigClient:   dcClient,
 		TLSConfigProvider:     tlsConfigProvider,
+		EsConfig:              esConfig,
 		EsClient:              esClient,
 		MetricsHandler:        metricHandler,
 	}, nil
